33bc4866f76567e3645c7dd3c78da360cf3805c3
Israel Soares <israeldiassoares@gmail.com>
2023-01-02 22:41:41 +0100

    adicionado checkbox toggle validacao

--- 08-forms/src/app/data-form/data-form.component.ts
+++ 08-forms/src/app/data-form/data-form.component.ts
@@ -66,7 +66,8 @@ export class DataFormComponent implements OnInit {
       }),
       cargo: [ null ],
       tecnologias: [ null ],
-      newsletter: [ 's' ]
+      newsletter: [ 's' ],
+      termos: [ null, Validators.pattern('true') ]
     })
   }
 
