fb5ffd14746e770542f7d12be990c4f42f0ebc90
Israel Soares <israeldiassoares@gmail.com>
2023-01-24 23:30:30 +0100

    integrated and validated curso form fields

--- requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
+++ requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-cursos-form',
   templateUrl: './cursos-form.component.html',
-  styleUrls: ['./cursos-form.component.css']
+  styleUrls: [ './cursos-form.component.css' ]
 })
 export class CursosFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  submitted: boolean
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = new FormGroup({})
+    this.submitted = false
+  }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: [ null, [ Validators.required, Validators.minLength(3), Validators.maxLength(250) ] ]
+    })
+  }
+
+  hasError(field: string): ValidationErrors | null | undefined {
+    return this.form.get(field)?.errors
+  }
+
+
+  onSubmit() {
+    this.submitted = true
+    console.log(this.form.value)
+    if (this.form.valid) {
+      console.log('submit')
+    }
+  }
+
+  onCancel() {
+    this.submitted = false
+    this.form.reset()
   }
 
 }
--- requests-http/src/app/cursos/cursos.module.ts
+++ requests-http/src/app/cursos/cursos.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 import { CursosFormComponent } from './cursos-form/cursos-form.component';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { CursosFormComponent } from './cursos-form/cursos-form.component';
   ],
   imports: [
     CommonModule,
-    CursosRoutingModule
+    CursosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CursosModule { }
