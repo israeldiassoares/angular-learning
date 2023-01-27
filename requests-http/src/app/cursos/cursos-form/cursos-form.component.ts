135f90e14df3183f2aa4b0897ddeb8e5c1a7fd9f
Israel Soares <israeldiassoares@gmail.com>
2023-01-27 21:41:38 +0100

    refactory routeParams Observable simplifying

--- requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
+++ requests-http/src/app/cursos/cursos-form/cursos-form.component.ts
@@ -4,6 +4,7 @@ import { CursosService } from './../cursos-lista/cursos.service'
 import { Component, OnInit } from '@angular/core'
 import { Location } from '@angular/common'
 import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms'
+import { map, switchMap } from 'rxjs'
 
 @Component({
   selector: 'app-cursos-form',
@@ -27,17 +28,35 @@ export class CursosFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    let registro = null //ERrado
 
-    this.route.params.subscribe(
-      (params: any) => {
-        const id = params[ 'id' ]
-        console.log('id', id)
-        const curso$ = this.service.loadById(id)
-        curso$.subscribe(curso => {
-          this.updateForm(curso)
-        })
-      }
+    //Trabalhando com observable nao temos uma certeza de quando o código será executado
+    //Codigo assincrono, nao sendo possivel iniciar valores dentro delea
+    // this.route.params.subscribe(
+    //   (params: any) => {
+    //     const id = params[ 'id' ]
+    //     console.log('id', id)
+    //     const curso$ = this.service.loadById(id)
+    //     curso$.subscribe(curso => {
+    //       // registro = curso //correto é utilizar o subscribe e utilizar o patchvalue para modificar o form
+    //       this.updateForm(curso)
+    //     })Codigo movido para refatoração, para diminuir a complexidade e
+    //   } melhoria
+    // )
+    //inicializar com null pois pode demorar alguns MS para executar|popular a variavel no codigo assincrono
+    // console.log('registro ', registro)
+    //Para a parte do angular devemos inicializar o formulario no constrc ou no ngOnInit
+
+    //subscrib in route.params angular automaticamente faz o unsubscribe o angular monitora
+    this.route.params.pipe(
+      map((params: any) => params[ 'id' ]),
+      switchMap(id => this.service.loadById(id))
+      // switchMap(cursos => obterAulas)
     )
+      .subscribe(curso => this.updateForm(curso))
+    //concatMap -> a ordem da requisição importa
+    // mergeMap -> nao importa a ordem de requisição
+    // exhaustMap -> vai fazer a requisicao e obtem a resposta antes da segunda tentativa, sincronamente req, espera a resp e parte para a segunda chamada comum em casos de login (CRUD)
 
     this.form = this.formBuilder.group({
       id: [ null ],
