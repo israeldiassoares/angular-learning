9ea378e1707abb30591da169606e274ca6ddf30a
Israel Soares <israeldiassoares@gmail.com>
2023-02-13 15:48:07 +0100

    created http|angular|passing URL params

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Observable, map, tap } from 'rxjs'
 import { Component, OnInit } from '@angular/core'
 import { FormControl } from '@angular/forms'
@@ -22,15 +22,35 @@ export class LibSearchComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  //TODO criar um checkbox visual para usuario filtrar os campos a ser mostrado
   onSearch() {
-    console.log(this.queryField.value)
-    //TODO criar servico para chamadas
-    this.results$ = this.http.get(this.SEARCH_URL + '?search=angular&fields=filename,description,version,github').pipe(
-      // TODO Criar interface para response
-      tap((res: any) => { this.total = res?.total }),
-      map(res => res.results)
-    )
-  }
+    const fields = `filename,description,version,github`
+    let value = this.queryField.value
+    if (value && (value = value.trim()) !== '') {
+      //http tradicional
+      // this.results$ = this.http.get(this.SEARCH_URL + `?search=${value}&fields=${fields}`).pipe(
+      //   // TODO Criar interface para response
+      //   tap((res: any) => { this.total = res?.total }),
+      //   map(res => res.results)
 
+
+      //methodo angular com parametros fixos
+      const paramsFixo = {
+        search: value,
+        fields: fields
+      }
+      //criacao de parametros dinamico
+      let params = new HttpParams()
+      params = params.set('search', value)
+      params = params.set('fields', fields)
+      // params.append('novoValor', novoValor)
+
+      //TODO criar servico para chamadas
+      this.results$ = this.http.get(this.SEARCH_URL, { params }).pipe(
+        // TODO Criar interface para response
+        tap((res: any) => { this.total = res?.total }),
+        map(res => res.results)
+      )
+    }
+  }
 }
