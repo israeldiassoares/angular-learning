befe803a6e6d5c446520109dbe639242dbe9fa9f
Israel Soares <israeldiassoares@gmail.com>
2023-02-15 22:31:35 +0100

    created firebase connection to deploy

--- requests-http/src/polyfills.ts
+++ requests-http/src/polyfills.ts
@@ -35,7 +35,8 @@ import '@angular/localize/init';
  *
  * The following flags will work for all browsers.
  *
- * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+ *
+ *  (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
  * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
  * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
  *
@@ -46,6 +47,11 @@ import '@angular/localize/init';
  *
  */
 
+  (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+  (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+  (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+
+(window as any).__Zone_enable_cross_context_check = true;
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
