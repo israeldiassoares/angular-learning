4e5c0d8be9ff2d79e1e8d37870d0570cad104f7e
Israel Soares <israeldiassoares@gmail.com>
2023-02-17 22:13:30 +0100

    created module Courses|routingModule|component

--- crud-angular-spring/crud-angular/src/app/app-routing.module.ts
+++ crud-angular-spring/crud-angular/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo:'courses' },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(cm => cm.CoursesModule) }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
---
+++ crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule
+  ]
+})
+export class CoursesModule { }
