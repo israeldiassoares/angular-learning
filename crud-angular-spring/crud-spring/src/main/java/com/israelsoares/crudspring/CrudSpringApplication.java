fcb92ffa94df92263240fcb59406a039f1163fa4
Israel Soares <israeldiassoares@gmail.com>
2023-02-19 22:33:05 +0100

    Tested created CourseData CLR InitDataBase|removedHello|GetMappingListAll

--- crud-angular-spring/crud-spring/src/main/java/com/israelsoares/crudspring/CrudSpringApplication.java
+++ crud-angular-spring/crud-spring/src/main/java/com/israelsoares/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.israelsoares.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.israelsoares.crudspring.model.Course;
+import com.israelsoares.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,20 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	// Spring inicializou, vai executar a lógica que tem dentro desse @bean
+	// (avisando q spring gerencie todo o ciclo de vida)
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course newCourse = new Course();
+
+			newCourse.setName("Angular Spring");
+
+			newCourse.setCategory("Front-End");
+
+			courseRepository.save(newCourse);
+		};
+	}
 }
--- crud-angular-spring/crud-spring/src/main/java/com/israelsoares/crudspring/controller/CourseController.java
+++ crud-angular-spring/crud-spring/src/main/java/com/israelsoares/crudspring/controller/CourseController.java
@@ -6,19 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.israelsoares.crudspring.model.Course;
 import com.israelsoares.crudspring.repository.CourseRepository;
 
 @RestController
 @RequestMapping("/api/courses")
-
+// @AllArgsConstructor Fazer injecao de dependencai vai lombok
 // @Component para gerencie o ciclo de vida como um componente
 public class CourseController {
 
-    private CourseRepository courseRepository;
+    // @Autowired fazer injeção de dependencia (usar o objeto sem se preocupar com a
+    // implementacao) ou DI via setters
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
     // @RequestMapping(method = RequestMethod.GET) msm coisa que o GetMapping
     @GetMapping
     public List<Course> list() {
-        return null;
+        return courseRepository.findAll();
     };
 }
