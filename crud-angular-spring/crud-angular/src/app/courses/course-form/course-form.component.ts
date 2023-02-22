226b4d0a68119967a8f82bab52c88b4568b9e1da
Israel Soares <israeldiassoares@gmail.com>
2023-02-22 15:37:42 +0100

    add ToastBarOnSuccess|BackToList|locationImportDI

--- crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-import { CoursesService } from './../services/courses.service'
-import { MatSnackBar } from '@angular/material/snack-bar'
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -16,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [ null ],
@@ -29,12 +31,22 @@ export class CourseFormComponent implements OnInit {
   onSubmit() {
     this.service.save(this.form.value)
       .subscribe(
-        data => console.log(data),
+        data => this.onSuccess("Curso salvo com sucesso"),
         error => this.onError("Erro ao salvar curso")
       )
   }
 
-  onCancel() { }
+  onCancel() {
+    this.location.back();
+   }
+
+  private onSuccess(msg: string) {
+    this._snackBar.open(
+      msg,
+      '',
+      { duration: 5000 })
+      this.onCancel();
+  }
 
   private onError(msg: string) {
     this._snackBar.open(
