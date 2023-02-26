0141c0542ca9bb71ee0df420abc26c390db81474
Israel Soares <israeldiassoares@gmail.com>
2023-02-26 22:35:09 +0100

    CreateDeleteEP|Tested|IntegratedFront

--- crud-angular-spring/crud-spring/src/main/java/com/israelsoares/crudspring/controller/CourseController.java
+++ crud-angular-spring/crud-spring/src/main/java/com/israelsoares/crudspring/controller/CourseController.java
@@ -28,32 +28,50 @@ public class CourseController {
     public @ResponseBody List<Course> list() {
         //public @ResponseBody List<Course> list() a lista é um response body e essa éa marcação no spring FasterXML responsável pelo Marshalling and Unmarshalling
         return courseRepository.findAll();
-    };
-  //  “Unmarshalling” is the process of converting some kind of a lower-level representation, often a “wire format”, into a higher-level (object) structure. Other popular names for it are “Deserialization” or “Unpickling”.
+    }
+
+    ;
+    //  “Unmarshalling” is the process of converting some kind of a lower-level representation, often a “wire format”, into a higher-level (object) structure. Other popular names for it are “Deserialization” or “Unpickling”.
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id){
-      return courseRepository.findById(id)
-              .map(recordFound -> ResponseEntity.ok().body(recordFound))
-              .orElse(ResponseEntity.notFound().build());
-    };
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    ;
 
     @PutMapping("/{id}")
     public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
-    return courseRepository.findById(id).map( recordFound -> {
-        recordFound.setName(course.getName());
-        recordFound.setCategory(course.getCategory());
-        Course updateCourse = courseRepository.save(recordFound);
-        return ResponseEntity.ok().body(updateCourse);
-    }).orElse(ResponseEntity.notFound().build());
-    };
-    
+        return courseRepository.findById(id).map(recordFound -> {
+            recordFound.setName(course.getName());
+            recordFound.setCategory(course.getCategory());
+            Course updateCourse = courseRepository.save(recordFound);
+            return ResponseEntity.ok().body(updateCourse);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    ;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id).map(recordFound -> {
+            courseRepository.deleteById(id);
+            return ResponseEntity.noContent().<Void>build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    ;
+
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping //@RequestMapping(method = RequestMethod.POST)
     public Course create(@RequestBody Course course) {
         // public ResponseEntity<Course> create(@RequestBody Course course)
         //System.out.println(course.getName());
-       //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
         return courseRepository.save(course);
-    };
+    }
+
+    ;
 }
