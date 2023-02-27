8e1c83c5adb5645ee3e9a507d5fe57de329808d1
Israel Soares <israeldiassoares@gmail.com>
2023-02-27 17:46:08 +0100

    validatedRequired|MinMaxlength|fieldsFront

--- crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,11 +1,11 @@
-import { Course } from './../../model/course'
-import { ActivatedRoute } from '@angular/router'
 import { Location } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
-import { FormBuilder, NonNullableFormBuilder, Validators } from '@angular/forms'
+import { NonNullableFormBuilder, Validators } from '@angular/forms'
 import { MatSnackBar } from '@angular/material/snack-bar'
+import { ActivatedRoute } from '@angular/router'
 
 import { CoursesService } from '../../services/courses.service'
+import { Course } from './../../model/course'
 
 @Component({
   selector: 'app-course-form',
@@ -16,8 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [ '' ],
-    name: [ '' ],
-    category: [ '' ]
+    name: [ '', [ Validators.required, Validators.minLength(3), Validators.maxLength(50) ] ],
+    category: [ '', [ Validators.required ] ]
   });
 
   constructor(
@@ -58,5 +58,23 @@ export class CourseFormComponent implements OnInit {
       msg,
       '',
       { duration: 5000 })
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName)
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório'
+    }
+    if (field?.hasError('minlength')) {
+      const requiredLength: number = field.errors ? field.errors[ 'minlength' ][ 'requiredLength' ] : 3
+      return `Deve conter mínimo de ${requiredLength} caracteres`
+    }
+    if (field?.hasError('maxlength')) {
+      const requiredLength: number = field.errors ? field.errors[ 'maxlength' ][ 'requiredLength' ] : 50
+
+      return `Deve conter máximo de ${requiredLength} caracteres`
+    }
+    return 'Campo inválido'
   }
 }
